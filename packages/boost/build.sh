028034a02119d5f2821dfbf9dacf7ac4b2385c4e
Yaksh Bariya <yakshbari4@gmail.com>
2024-06-29 17:41:16 +0530

    bump(main/boost): 1.85.0

--- packages/boost/build.sh
+++ packages/boost/build.sh
@@ -4,10 +4,9 @@ TERMUX_PKG_LICENSE="BSL-1.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # Never forget to always bump revision of reverse dependencies and rebuild them
 # when bumping version.
-TERMUX_PKG_VERSION="1.83.0"
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://boostorg.jfrog.io/artifactory/main/release/$TERMUX_PKG_VERSION/source/boost_${TERMUX_PKG_VERSION//./_}.tar.bz2
-TERMUX_PKG_SHA256=6478edfe2f3305127cffe8caf73ea0176c53769f4bf1585be237eb30798c3b8e
+TERMUX_PKG_VERSION="1.85.0"
+TERMUX_PKG_SRCURL=https://archives.boost.io/release/$TERMUX_PKG_VERSION/source/boost_${TERMUX_PKG_VERSION//./_}.tar.bz2
+TERMUX_PKG_SHA256=7009fe1faa1697476bdc7027703a2badb84e849b7b0baad5086b087b971f8617
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="libc++, libbz2, libiconv, liblzma, zlib"
 TERMUX_PKG_BUILD_DEPENDS="python"
