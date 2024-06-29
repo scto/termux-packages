5cb0aae66fb790503d00f56ba4087c47ec395aca
Yaksh Bariya <yakshbari4@gmail.com>
2024-06-29 17:42:13 +0530

    tmp commit

--- packages/lgogdownloader/build.sh
+++ packages/lgogdownloader/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Open source downloader to GOG.com for Linux users using 
 TERMUX_PKG_LICENSE="WTFPL"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.14"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/Sude-/lgogdownloader/releases/download/v${TERMUX_PKG_VERSION}/lgogdownloader-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=2629c54e2549881cdd155b4f89b472c62617db7a9cfe6b68dbcb1753080b1448
 TERMUX_PKG_AUTO_UPDATE=true
